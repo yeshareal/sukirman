9133767ebbcf47cf5c6d91b61440914611220133
Cann <69559214+yeshareal@users.noreply.github.com>
2020-11-04 00:47:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://sinnerz.glitch.me"]
+const urls = ["https://sukirman.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
